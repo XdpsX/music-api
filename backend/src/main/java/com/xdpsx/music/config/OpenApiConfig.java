9a286b274a065b75e252cde9d0165561de999e27
Night <dranhata@gmail.com>
2024-08-26 10:24:31 +0700

    update servers in docs

--- backend/src/main/java/com/xdpsx/music/config/OpenApiConfig.java
+++ backend/src/main/java/com/xdpsx/music/config/OpenApiConfig.java
@@ -1,41 +1,53 @@
 package com.xdpsx.music.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
-        info = @Info(
-                title = "Music REST APIs",
-                description = "Documentation for Music REST APIs",
-                version = "1.0",
-                license = @License(
-                        name = "Licence name",
-                        url = "https://some-url.com"
-                ),
-                termsOfService = "Terms of service"
-        ),
-        servers = {
-                @Server(
-                        description = "Local ENV",
-                        url = "http://localhost:8080${server.servlet.context-path}"
-                ),
-                @Server(
-                        description = "Deployment ENV",
-                        url = "${app.url}${server.servlet.context-path}"
-                )
-        }
-)
-@SecurityScheme(
-        name = "JWT",
-        scheme = "bearer",
-        type = SecuritySchemeType.HTTP,
-        bearerFormat = "JWT",
-        in = SecuritySchemeIn.HEADER
-)
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
 public class OpenApiConfig {
+
+    @Value("${server.servlet.context-path:/}")
+    private String contextPath;
+
+    @Value("${app.url:}")
+    private String appUrl;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        OpenAPI openAPI = new OpenAPI()
+                .info(new Info()
+                        .title("Music REST APIs")
+                        .description("Documentation for Music REST APIs")
+                        .version("1.0")
+                        .license(new License().name("Licence name").url("https://some-url.com"))
+                        .termsOfService("Terms of service")
+                )
+                .addSecurityItem(new io.swagger.v3.oas.models.security.SecurityRequirement().addList("JWT"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("JWT", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER))
+                );
+
+        List<Server> servers = new ArrayList<>();
+
+        if (!appUrl.isEmpty()) {
+            servers.add(new Server().description("Prod ENV").url(appUrl + contextPath));
+        }else {
+            servers.add(new Server().description("Local ENV").url("http://localhost:8080" + contextPath));
+        }
+
+        return openAPI.servers(servers);
+    }
 }
